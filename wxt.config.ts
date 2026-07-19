70c649d1af0bffc748d4b34683970ba500e193ac
Sri Pravan Paturi <pravanpaturi@gmail.com>
2026-07-19 12:14:24 +0530

    Added appropriate images for different icon sizes

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,6 +7,17 @@ export default defineConfig({
     name: "Infibox",
     description: "The ultimate browser extension for developer productivity",
     permissions: ["contextMenus", "scripting", "storage"],
+    icons: {
+      "16": "img/infibox-16.png",
+      "48": "img/infibox-48.png",
+      "128": "img/infibox-128.png",
+    },
+    action: {
+      default_icon: {
+        "16": "img/infibox-16.png",
+        "32": "img/infibox-32.png",
+      },
+    },
     omnibox: {
       keyword: "infi",
     },
